197130a08cef4f9561937e19c4fc60d112f0b206
unknown <ashutoshth456@gmail.com>
2025-03-05 21:51:25 +0545

    auth wraper

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<AuthWrapper><Dashboard /> </AuthWrapper>} />
+        <Route path="/dashboard" element={<Dashboard /> } />
       </Routes>
     </Router>
   );
